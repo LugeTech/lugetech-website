430964337d14a63503d3fa82aab7b5f09335fc10
Ken Taylor <kentaylosappdev@gmail.com>
2024-01-18 13:11:26 -0400

    fixed menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen md:w-8/12 mx-auto p-4`}>
+      <body className={`${inter.className} h-screen md:w-10/12 mx-auto p-4`}>
         {children}
       </body>
     </html>
