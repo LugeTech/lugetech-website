8f9917a27d5db268563319ebc64c9430b04cd619
Ken Taylor <kentaylosappdev@gmail.com>
2024-01-17 23:23:04 -0400

    wonderful server actions to get pinned repos

--- src/app/lib/serverActions.ts
+++ src/app/lib/serverActions.ts
@@ -1,25 +1,22 @@
-'use server'
-import { profile, Repo } from "./interfaces"
-import { getPinnedRepos } from '@kentaylorappdev/get-pinned-repos'
+"use server";
+import { profile, Repo } from "./interfaces";
+import { getPinnedRepos_v2 } from "@kentaylorappdev/get-pinned-repos";
 
 export const GetGithubProfile = async (username: string): Promise<profile> => {
   return fetch(`https://api.github.com/users/${username}`)
     .then((res) => res.json())
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 export const GetGithubRepos = async (username: string): Promise<Repo[]> => {
   return fetch(`https://api.github.com/users/${username}/repos`)
     .then((res) => res.json())
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
-
-export const GetGithubPinnedRepos = async (username: string): Promise<Repo[]> => {
-  const pinned = await getPinnedRepos(username) as unknown as String[]
-  const allrepos = await GetGithubRepos(username)
-
-  return pinned.map((pinnedRepo) => {
-    return allrepos.find((repo: Repo) => repo.full_name === pinnedRepo) as Repo
-  })
-}
+export const GetGithubPinnedRepos = async (username: string) => {
+  console.log("here");
+  const pinned = await getPinnedRepos_v2(username);
+  console.log(pinned);
+  return pinned;
+};
